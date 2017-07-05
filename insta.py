974f1d6a7d7ea853d8f6b65540267bbfb745cd72
snehabhuyan <sneha.personified@gmail.com>
2017-07-05 09:36:16 +0530

    get the user details

---
+++ insta.py
@@ -0,0 +1,95 @@
+import requests
+
+APP_ACCESS_TOKEN =  '1705754005.29541b0.156ba485dedb482cbc95c8e430efa9ec'
+#Token Owner : snehabhuyan
+#Sandbox Users : mahak_sachdeva,khattarsakshi,
+
+BASE_URL = 'https://api.instagram.com/v1/'
+
+'''
+Function declaration to get your own info
+'''
+def self_info():
+    request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'User does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+'''
+Function declaration to get the ID of a user by username
+'''
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+'''
+Function declaration to get the info of a user by username
+'''
+
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
+
+def start_bot():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your options available:'
+        print "1.Get your own details\n"
+        print "2.Get details of a user by username\n"
+
+
+
+        print "3.Exit"
+
+        choice=raw_input("Enter you choice: ")
+        if choice=="1":
+            self_info()
+        elif choice=="2":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_info(insta_username)
+
+
+        elif choice=="3":
+            exit()
+        else:
+            print "wrong choice"
+
+start_bot()
